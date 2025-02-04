e1017decae19be70029be9e5879e264538602741
Makwana MIhir <makwanamihir2023.katargam@gmail.com>
2025-02-04 15:10:29 +0530

    Update category.js
    category Update

--- routes/category.js
+++ routes/category.js
@@ -5,6 +5,7 @@ const categorycontroller = require('../controller/categorycontroller')
 /* GET users listing. */
 router.get('/', categorycontroller.Read);
 router.post('/', categorycontroller.Create);
-router.patch('/:id', categorycontroller.Create);
+router.patch('/:id', categorycontroller.Update);
+router.delete('/:id', categorycontroller.Delete);
 
 module.exports = router;
